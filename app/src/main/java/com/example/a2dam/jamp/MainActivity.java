8fbe899f0bd71b1340ae965545553c79d51ac4e7
Julen <julenzarate@gmail.com>
2018-10-20 13:22:05 +0200

    Modificacion de la ventan Principal y mas cosas necesarias

--- app/src/main/java/com/example/a2dam/jamp/MainActivity.java
+++ app/src/main/java/com/example/a2dam/jamp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.a2dam.jamp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInicio;
+    EditText pfContraseña;
+    TextView lblError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pfContraseña = findViewById(R.id.pfContraseña);
+        btnInicio = (Button) findViewById(R.id.btnInicio);
+
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent iniciarSesion = new Intent(MainActivity.this, PrincipalActivity.class);
+                startActivity(iniciarSesion);
+            }
+        });
     }
+
+
 }
