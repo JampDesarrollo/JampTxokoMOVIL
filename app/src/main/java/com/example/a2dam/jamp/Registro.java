3a2f3ca0d41ff68b204994bcd0ddd1330233a847
2dam <desarrolojamp@gmail.com>
2018-10-22 10:27:31 +0200

    Codificacion Registro(Metodos Vacios)

--- app/src/main/java/com/example/a2dam/jamp/Registro.java
+++ app/src/main/java/com/example/a2dam/jamp/Registro.java
@@ -1,16 +1,19 @@
 package com.example.a2dam.jamp;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class Registro extends AppCompatActivity implements View.OnClickListener,{
+public class Registro extends AppCompatActivity implements View.OnClickListener{
 
     boolean correcto=false;
+    TextView textLogin =findViewById(R.id.tfLogin);
+    TextView textFullName =findViewById(R.id.tfFullName);
+    TextView texteMail =findViewById(R.id.tfeMail);
+    EditText pass1=findViewById(R.id.pfPassword1);
+    EditText pass2=findViewById(R.id.pfPassword2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,28 +27,28 @@ public class Registro extends AppCompatActivity implements View.OnClickListener,
     }
 
     private void controlarTodosLosCampos() {
-        if(R.id.tfLogin>0){
-            if(R.id.tfFullName>0){
-                if(R.id.tfeMail>0){
-                    if(R.id.pfPassword1>0){
-                        if(R.id.pfPassword2>0){
+        if(textLogin.getText().length()>0){
+            if(textFullName.getText().length()>0){
+                if(texteMail.getText().length()>0){
+                    if(pass1.getText().length()>0){
+                        if(pass2.getText().length()>0){
                             contraseñasIguales();
-                        }else{dialogoError("La Segunda Contrasaeña Esta Vaacia")}
-                    }else{dialogoError("La Primera Contraseña Esta Vacia")}
-                }else{dialogoError("El eMail Esta Vacio")}
-            }else{dialogoError("El Nombre Esta Vacio")}
+                        }else{cambiarlblComprobante("La Segunda Contrasaeña Esta Vaacia");}
+                    }else{cambiarlblComprobante("La Primera Contraseña Esta Vacia");}
+                }else{cambiarlblComprobante("El eMail Esta Vacio");}
+            }else{cambiarlblComprobante("El Nombre Esta Vacio");}
         comprobarDatos();
-        }else{dialogoError("El Login Esta Vacio")}
+        }else{cambiarlblComprobante("El Login Esta Vacio");}
     }
 
     public void cambiarlblComprobante(String mensaje){
         TextView comprobante=findViewById(R.id.lblComprobante);
         comprobante.setText(mensaje);
-        comprobante.setTextColor(@color/rojo);
+        comprobante.setTextColor(getResources().getColor(R.color.rojo));
     }
 
     private void contraseñasIguales() {
-        if(R.id.pfPassword2!=R.id.pfPassword1){
+        if(pass1!=pass2){
             cambiarlblComprobante("Las Contraseñas No Coinciden");
         }
     }
