58c969adfa34d3c84f5443187283604c2ea1a76e
markelonate <markel.onate@gmail.com>
2019-01-14 08:25:12 +0100

    AndroidManifest.xml añadida la linea para que no muestre el teclado EncryptPassword.java añadida la excepcion nullpointer

--- app/src/main/java/com/example/a2dam/jamp/model/PrincipalActivity.java
+++ app/src/main/java/com/example/a2dam/jamp/model/PrincipalActivity.java
@@ -59,8 +59,7 @@ public class PrincipalActivity extends AppCompatActivity
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
 
         toggle.syncState();
