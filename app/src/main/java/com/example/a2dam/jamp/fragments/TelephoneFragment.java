8344c2117eb9af7d48ebe268cef3853efe61ad03
markelonate <markel.onate@gmail.com>
2019-01-28 19:40:52 +0100

    Mongo con telefonos a medias comentarios de los adapters librerias de mongo

--- app/src/main/java/com/example/a2dam/jamp/fragments/TelephoneFragment.java
+++ app/src/main/java/com/example/a2dam/jamp/fragments/TelephoneFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,9 +19,17 @@ import com.example.a2dam.jamp.adapters.AdapterTelephone;
 import com.example.a2dam.jamp.dataClasses.Telephone;
 import com.example.a2dam.jamp.R;
 import com.example.a2dam.jamp.models.PrincipalActivity;
-
-
+import com.example.a2dam.jamp.threads.ThreadForSocketTelephone;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 import java.util.ArrayList;
+import org.bson.Document;
+import java.util.ArrayList;
+
 
 
 /**
@@ -33,8 +42,11 @@ public class TelephoneFragment extends Fragment implements View.OnClickListener,
     private EditText search;
     private ImageButton btnSearch;
     private ListView lv;
-    TextView telephoneError;
+    private TextView telephoneError;
     private View view;
+    private static MongoClient mongoclient;
+    private static MongoDatabase mongoDB;
+    private static MongoCollection<Document> collection;
 
     private OnFragmentInteractionListener mListener;
 
@@ -45,24 +57,36 @@ public class TelephoneFragment extends Fragment implements View.OnClickListener,
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //inflar la vista para poder referenciar los elementos declarados arriba
         view = inflater.inflate(R.layout.fragment_telephone, container, false);
+
+        //establecer el titulo del fragment en la barra superior
         ((PrincipalActivity) getActivity()).getSupportActionBar().setTitle(R.string.fragment_telephones_titulo);
 
+        //referenciar y escuchar el boton de busqueda
         btnSearch=view.findViewById(R.id.btnSearchTelephone);
         btnSearch.setOnClickListener(this);
 
+        //referenciar el campo de texto de busqueda
         search=view.findViewById(R.id.tfSearchTelephoneName);
+
         //referenciar el listview
         telephoneError= view.findViewById(R.id.lblSearchTelephoneError);
 
+        //declaramos un arraylist de telefonos y lo inicializamos llamando al metodo de cargar telfonos
         ArrayList<Telephone> telephones = cargarTelefonos();
-        if(telephones.isEmpty()) {
+
+
+        if(telephones.isEmpty()) {//si el metodo cargar telefonos devuelve un array vacio
+            //se hace visible un mesaje de error oculto
             telephoneError.setVisibility(View.VISIBLE);
-        }else{
+        }else{//si el array esta lleno
+            //referenciar el list view
             lv = view.findViewById(R.id.TelephoneListView);
-            //crear un nuevo tipo de dato adapterevents y pasamos el array
+            //crear un nuevo tipo de dato adapterevents y lo inicializamos con el mismo y el array que tenemos lleno
             AdapterTelephone adapter = new AdapterTelephone(this, telephones);
-            //llamamos al setadapter del listview con el adapter que hemos creado antes
+            //llamamos al setadapter del listview con el adapter que hemos creado antes para llenar el listview con todos los items del array
             lv.setAdapter(adapter);
             //definimos el onintemclick
             lv.setOnItemClickListener(this);
@@ -74,12 +98,16 @@ public class TelephoneFragment extends Fragment implements View.OnClickListener,
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btnSearchTelephone:
+            case R.id.btnSearchTelephone://si clicamos en el boton de buscar
+                //pinta de blanco la linea del campo de busqueda por si estuviese roja de otra busqueda
                 search.setBackgroundTintList(this.getResources().getColorStateList(R.color.blanco));
-                if(search.getText().toString().trim().isEmpty()){
+                if(search.getText().toString().trim().isEmpty()){//si el campo esta vacio
+                    // muestra el error del campo requerido
                     search.setError(this.getResources().getString(R.string.field_requiered_error));
+                    //pinta la linea inferior del campo de busqueda de rojo
                     search.setBackgroundTintList(this.getResources().getColorStateList(R.color.rojo));
-                }else{
+                }else{//si el campo esta lleno
+                    //llamamos al metodo de cargar los datos con la condicional del campo de busqueda
                     cargarTelefonosCondicional();
                 }
                 break;
@@ -87,16 +115,53 @@ public class TelephoneFragment extends Fragment implements View.OnClickListener,
     }
 
     private ArrayList<Telephone> cargarTelefonos() {
-        ArrayList<Telephone> telephone = new ArrayList<>();
-        for(int i=0; i<20; i++){
+        ArrayList <Telephone> telephones=new ArrayList<>();
+
+
+        //Declarar all por separado
+        mongoclient = MongoClients.create("mongodb://192.168.0.15:27017/jamp");
+        mongoDB = mongoclient.getDatabase("jamp");
+        collection = mongoDB.getCollection("telephones");
+
+        FindIterable<Document> fi;
+        MongoCursor<Document> cursor;
+        fi = collection.find();
+        cursor = fi.iterator();
+        Document documento;
+
+        Telephone tel = null;
+        try {
+            if(telephones.isEmpty()) {
+                lv.setAdapter(null);
+                telephoneError.setVisibility(View.VISIBLE);
+            }
+            while (cursor.hasNext()) {
+                documento = cursor.next();
+                //crear un documeto para que no carge el siguiente
+                tel.setId(documento.getInteger("id"));
+                tel.setDescription(cursor.next().getString("descripción"));
+                tel.setNombre(cursor.next().getString("name"));
+                tel.setTelephon(cursor.next().getInteger("telephone"));
+                telephones.add(tel);
+            }
+        }catch(Exception e) {
+            Log.d("error","la exception: "+ e);
+        } finally {
+            cursor.close();
+        }
+
+        /*
+        for (int i = 0; i < 20; i++) {
             Telephone tel = new Telephone();
 
-            tel.setNombre("Telepizza" +" "+i);
-            tel.setTelephon(944644465 + i);
+            tel.setNombre("Telepizza" + " " + i);
+            tel.setTelephon(944644465);
 
-            telephone.add(tel);
-        }
-        return telephone;
+            telephones.add(tel);
+
+        }*/
+
+        return telephones;
     }
 
     private void cargarTelefonosCondicional() {
--- app/src/main/java/com/example/a2dam/jamp/threads/ThreadForSocketTelephone.java
+++ app/src/main/java/com/example/a2dam/jamp/threads/ThreadForSocketTelephone.java
@@ -1,29 +1,37 @@
 package com.example.a2dam.jamp.threads;
 
+import android.util.Log;
+import android.view.View;
+
+import com.example.a2dam.jamp.dataClasses.Telephone;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+import java.util.ArrayList;
+
 public class ThreadForSocketTelephone extends Thread {/*
-    private TelephoneBean telephone;
-    private TelephoneLogic ilogic;
+    private ArrayList<Telephone> telephones;
     private int code;
 
-    public TelephoneBean getTelephone(){
-        return telephone;
-    }
-
-    public ThreadForSocketTelephone(TelephoneBean receivedTelephone, TelephoneLogic ilogic, int i){
-        this.telephone=receivedTelephone;
-        this.ilogic=ilogic;
+    public ThreadForSocketTelephone(ArrayList<Telephone> receivedTelephones, int i){
+        this.telephones=receivedTelephones;
         this.code=i;
-    }
-*/
+    }*/
+
     /**
      * Method that start the thread
      */
     @Override
     public void run() {
-        /*switch (code){
+       /* switch (code){
             case 1:
                 try {
-                    ilogic.findAllTelephones(telephone);
+
                 }catch (Exception e){
                     throw new RuntimeException(e);
                 }
